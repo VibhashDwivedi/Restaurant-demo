0884fdd593d1cf0d07a0192b456a75d71360de06
VibhashDwivedi <vibhashdwi1005@gmail.com>
2024-02-04 19:21:27 +0530

    Refactor Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from 'react';
+
+import fineHeader from '../media/fine-footer.png';
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">
-      Navbar
-    </a>
+        <nav className="navbar navbar-expand-lg nav-back pt-0" >
+  <div className="container-fluid" >
+    <Link className="navbar-brand" to="">
+      <img src={fineHeader} alt="" className='fine-footer mx-5 px-1 pt-2' />
+    </Link>
     <button
       className="navbar-toggler"
       type="button"
@@ -19,72 +22,40 @@ const Navbar = () => {
     >
       <span className="navbar-toggler-icon" />
     </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">
-            Home
-          </a>
+    <div className="collapse navbar-collapse " id="navbarSupportedContent">
+      <ul className="navbar-nav ms-auto mb-2 mb-lg-0  nav-border ">
+        <li className="nav-item p-2 pt-3 nav-border-remove">
+          <NavLink className="nav-link  nav-link-css"  to="#" >
+            HOME
+          </NavLink>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">
-            Link
-          </a>
+        <li className="nav-item p-2 pt-3 ">
+          <NavLink className="nav-link nav-link-css"  to="#" >
+            ABOUT
+          </NavLink>
         </li>
-        <li className="nav-item dropdown">
-          <a
-            className="nav-link dropdown-toggle"
-            href="#"
-            id="navbarDropdown"
-            role="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            Dropdown
-          </a>
-          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li>
-              <a className="dropdown-item" href="#">
-                Action
-              </a>
-            </li>
-            <li>
-              <a className="dropdown-item" href="#">
-                Another action
-              </a>
-            </li>
-            <li>
-              <hr className="dropdown-divider" />
-            </li>
-            <li>
-              <a className="dropdown-item" href="#">
-                Something else here
-              </a>
-            </li>
-          </ul>
+        <li className="nav-item p-2 pt-3 ">
+          <NavLink className="nav-link nav-link-css"  to="#" >
+            MENU
+          </NavLink>
         </li>
-        <li className="nav-item">
-          <a
-            className="nav-link disabled"
-            href="#"
-            tabIndex={-1}
-            aria-disabled="true"
-          >
-            Disabled
-          </a>
+        <li className="nav-item p-2 pt-3 ">
+          <NavLink className="nav-link nav-link-css"  to="#" >
+            CHEFS
+          </NavLink>
+        </li>
+        <li className="nav-item p-2 pt-3 ">
+          <NavLink className="nav-link nav-link-css"  to="#" >
+            BOOK A TABLE
+          </NavLink>
+        </li>
+        <li className="nav-item p-2 pt-3 " style={{marginRight: 15}} >
+          <Link className="nav-link nav-link-css"  to="#" >
+            CONTACT US
+          </Link>
         </li>
       </ul>
-      <form className="d-flex">
-        <input
-          className="form-control me-2"
-          type="search"
-          placeholder="Search"
-          aria-label="Search"
-        />
-        <button className="btn btn-outline-success" type="submit">
-          Search
-        </button>
-      </form>
+    
     </div>
   </div>
 </nav>
