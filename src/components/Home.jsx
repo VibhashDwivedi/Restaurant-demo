7e4a1a8d33c7c08cd49c60a5c36739fe42fd3d6f
VibhashDwivedi <vibhashdwi1005@gmail.com>
2024-02-04 19:21:56 +0530

    Add Home component and associated CSS styles

---
+++ src/components/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import '../css/home.css'
+
+const Home = () => {
+  return (
+    <div className='main'>
+
+        <div className="container-fluid">
+            <div className="d-flex justify-content-center align-items-center vh-100">
+
+                <div>
+                <p className="text-white tagline mt-5">A Fancy & Vibrant Dining Experience</p>
+                <p className="text-white head pt-0 mt-0">TASTY LANE</p>
+                </div>
+                       
+                    
+                    
+            </div>
+            </div>
+
+    </div>
+  )
+}
+
+export default Home
